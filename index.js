961df5870195dbfa473be23d0039f55106403dcb
Edonak <edouanakasila69@gmail.com>
2024-03-27 10:59:34 +0000

    mis en place des endpoint

--- index.js
+++ index.js
@@ -13,9 +13,77 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 
-// router.get('/', function(req, res){
-//   getAllOrdinateurs().then(Ordinateurs => res.json(Ordinateurs))
-// })
+// Un endpoint /login pour l’authentification des utilisateurs
+// Route pour la page de login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Route pour l'authentification
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }); // Rechercher l'utilisateur dans la base de données
+  if (!user) {
+    return res.status(401).json({ error: 'Utilisateur non trouvé' });  // Si l'utilisateur n'existe pas, retourner une erreur
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password); // Comparer le mot de passe saisi avec le mot de passe haché dans la base de données
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: 'Mot de passe incorrect' }); // Si le mot de passe est incorrect, retourner une erreur
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  }); // Générer un token JWT
+  res.json({ token }); // Envoyer le token à l'utilisateur
+});
+
+module.exports = router;
+
+// Un endpoint /logout pour la déconnection des utilisateurs
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  // Rediriger l'utilisateur vers la page d'accueil
+  res.redirect('/');
+});
+module.exports = router;
+
+
+// Un endpoint /register pour la création de comptes utilisateurs
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+// Route pour la création de compte
+router.post('/register', async (req, res) => {
+  const { email, password, name } = req.body;
+
+  // Vérifier si l'utilisateur existe déjà
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ error: 'Utilisateur déjà existant' });
+  } // Si l'utilisateur existe déjà, retourner une erreur
+
+  const hashedPassword = await bcrypt.hash(password, 10);// Hacher le mot de passe
+  const user = new User({
+    email,
+    password: hashedPassword,
+    name,
+  }); // Créer un nouvel utilisateur
+  // Enregistrer l'utilisateur dans la base de données
+  await user.save();
+  // Générer un token JWT
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  // Envoyer le token à l'utilisateur
+  res.json({ token });
+});
+
+module.exports = router;
+
+router.get('/', function(req, res){
+  getAllOrdinateurs().then(Ordinateurs => res.json(Ordinateurs))
+})
 
 
 
