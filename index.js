ffa7ac68268e81c5b2ae23ceac9b6d0040718339
Edonak <edouanakasila69@gmail.com>
2024-03-27 09:57:00 +0000

    mis en place des endpoint

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+const router = express.Router();
+const {PrismaClient} = require('@PrismaClient');
 const app = express(); 
 const PORT = process.env.PORT || 8000;
 
 
-
-
+// router.get('/', function(req, res){
+//   getAllOrdinateurs().then(Ordinateurs => res.json(Ordinateurs))
+// })
 
 
 
